09c1378f5e3bf860e51f88c3189d084d8bd9676c
Minotto01 <gabriel.minotto5@gmail.com>
2023-05-10 18:42:23 -0300

    First Commit

---
+++ src/main/java/com/minotto01/crm/MinottocrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.minotto01.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinottocrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinottocrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minotto01/crm/MinottocrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minotto01.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinottocrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
